261ccee835d4b677ec962216fe290729a5f8830c
ICDLwebmaster <67035146+ICDLwebmaster@users.noreply.github.com>
2021-02-11 00:54:50 +0900

    newchange02/10

--- js/app.js
+++ js/app.js
@@ -1,37 +1,51 @@
 $('.slider').slick({
-  dots: false,
-  infinite: true,
-  speed: 1800,
-  slidesToShow: 1,
-  centerMode: true,
-  arrows: true,
-  autoplay: true,
-  autoplaySpeed: 500,
-  focusOnSelect: true,
-  variableWidth: true,
-  prevArrow: '<button class="prev-arrow slick-arrow"></button>',
-  nextArrow: '<button class="next-arrow slick-arrow"></button>',
-  responsive: [{
-  breakpoint: 960,
-    settings: {
-      slidesToShow: 1,
-      slidesToScroll: 1,
-      variableWidth: false,
-      centerPadding: '15px',
-    }
-  }]
+    dots: false,
+    infinite: true,
+    speed: 1800,
+    slidesToShow: 1,
+    centerMode: true,
+    arrows: true,
+    autoplay: true,
+    autoplaySpeed: 500,
+    focusOnSelect: true,
+    variableWidth: true,
+    prevArrow: '<button class="prev-arrow slick-arrow"></button>',
+    nextArrow: '<button class="next-arrow slick-arrow"></button>',
+    responsive: [{
+        breakpoint: 960,
+        settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            variableWidth: false,
+            centerPadding: '15px',
+        }
+    }]
 });
 
 $('.submylist-tab li').each(function(e) {
-  var t = $(this).attr('id');
-  $(this).on("click", function(e) {
-      $('#' + t + '-show').css('display', 'block');
-      $('.submylist-wrap').children('#' + t + '-show').siblings().css('display', 'none');
-      $(this).addClass("tab-active");
-      $(this).siblings().removeClass("tab-active");
-  });
+    var t = $(this).attr('id');
+    $(this).on("click", function(e) {
+        $('#' + t + '-show').css('display', 'block');
+        $('.submylist-wrap').children('#' + t + '-show').siblings().css('display', 'none');
+        $(this).addClass("tab-active");
+        $(this).siblings().removeClass("tab-active");
+    });
 });
 
 $('.logout-close').click(function() {
-  $('.logout').fadeOut();
+    $('.logout').fadeOut();
+});
+
+
+jQuery(function($) {
+    $(window).on("scroll", function() {
+        if ($(this).scrollTop() > 65) {
+            $(".subpage-l-hd").css({
+                "top": "0",
+                "position": "sticky",
+            });
+        } else {
+            $(".subpage-l-hd").css({});
+        }
+    });
 });
